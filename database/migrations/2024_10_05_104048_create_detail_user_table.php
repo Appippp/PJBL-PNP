e4f7f9dd1a9d07f92be792ff49c8a8ca7f7e0bb9
apppiiipp <hafifsyahputra2506@gmail.com>
2024-10-05 21:13:47 +0700

    first commit

---
+++ database/migrations/2024_10_05_104048_create_detail_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->index('fk_detail_user_to_users');
+            $table->foreignId('type_user_id')->nullable()->index('fk_detail_user_to_type_user');
+            $table->string('contact')->nullable();
+            $table->string('address')->nullable();
+            $table->string('gender')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_user');
+    }
+};
